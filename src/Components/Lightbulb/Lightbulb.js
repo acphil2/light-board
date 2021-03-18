e1287f64b6fcaa9baece8f06d344f3c5538c95dd
A C <72420910+acphil2@users.noreply.github.com>
2021-03-18 15:27:24 -0700

    changes

--- src/Components/Lightbulb/Lightbulb.js
+++ src/Components/Lightbulb/Lightbulb.js
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
+
+import React, { useState } from 'react';
 import './Lightbulb.css';
 
 const Lightbulb = (props) => {
 
-	const [isActive, setActive] = useState('false');
-  
+  const [isActive, setActive] = useState('false');
   const handleToggle = () => {
-	   setActive(!isActive);
+     setActive(!isActive);
      console.log(props.color)
-	};
+  };
 
   return (
     <div 
-      className={`lightbulb ${isActive ? null : 'lightbulbRed'}`}
+      className={`lightbulb ${isActive ? null : `lightbulb${props.color}`}`}
       onClick={handleToggle}
     ></div>
   );  
 }
 
 export default Lightbulb;
-
